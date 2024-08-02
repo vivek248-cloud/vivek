38484980f8da0bddc1edae4140bd5a4dab8ae343
vivek248-cloud <vivekofficial248@gmail.com>
2024-08-02 20:13:59 +0530

    Add files via upload

---
+++ web designing/new.js
@@ -0,0 +1,46 @@
+// /*function d( apple,straberry,orange)
+// {
+//     var total=apple + straberry + orange
+//     console.log("hii  " + total)
+//     return (total)
+// }
+// var apple=10
+// var straberry=20
+// var orange=15
+
+
+// var a=d( apple,straberry,orange)
+// console.log("hii  " + a)*/
+// function i( ball,red_ball,blue_ball)
+// {
+//     if (ball >= 7)
+//     {
+//         console.log("you have "+ball+" balls")
+//     }
+//     else
+//     {
+       
+//        e(red_ball,blue_ball)
+        
+//     }
+// }
+// function e(red_ball,blue_ball)
+// {
+//     var total = (red_ball) + (blue_ball)
+//     console.log("now you have '"+red_ball+"' red balls and '"+blue_ball+"' blue balls")
+//     console.log(total +" balls")
+// }
+// var red_ball=4
+// var blue_ball=3
+// var ball=5
+// i(ball,red_ball,blue_ball)
+var a=(document.getElementById("name").value)
+var b=(document.getElementById("passwore").value)
+if (a =='')
+{
+    alert("please fill the reqired feild!!")
+}
+else
+{
+    alert("successfully login !!")
+}
